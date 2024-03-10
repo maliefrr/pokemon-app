3ae468a5e7158b12d36ba3a3a0ffe7ebec5ef1d0
Mohamad Alief Rizky Ramadhan <maliefrr14@gmail.com>
2024-03-10 12:26:27 +0700

    feat: add routes

---
+++ src/routes/App.tsx
@@ -0,0 +1,17 @@
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import Home from '@/Pages/Home'
+
+
+const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Home/>
+    },
+    
+])
+
+const App = () => {
+  return <RouterProvider router={router}/>
+}
+
+export default App
